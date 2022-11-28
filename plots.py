386d7472fba8cbb8491267a25cdb2685a8c4fba5
System Administrator <root@carlos-air.students.clients.local>
2022-11-28 14:14:09 +0100

    Added FID best comparison

--- plots.py
+++ plots.py
@@ -18,4 +18,24 @@ plt.legend()
 plt.savefig("results/fid_training_comparasion.png")
 
 
+fid_wgangp = genfromtxt('results/fid_wgangp.csv', delimiter=',')
+fid_gansn = genfromtxt('results/fid_gansn.csv', delimiter=',')
+fid_gan = genfromtxt('results/fid_gan.csv', delimiter=',')
+n=3
+r = np.arange(n)
+width = 0.20
+fig = plt.figure(figsize=(6, 4), dpi=100)
+plt.bar(r, fid_wgangp, color = 'b', width = width, edgecolor = 'black', label='WGAN-GP')
+plt.bar(r + width, fid_gansn, color = 'y', width = width, edgecolor = 'black', label='GAN-SN')
+plt.bar(r + width * 2, fid_gan, color = 'r', width = width, edgecolor = 'black', label='Vanilla GAN')
+plt.xlabel("Taskset")
+plt.ylabel("Cost")
+plt.title("FID value of our models on MNIST, FashionMNIST and CelebA datasets")
+plt.xticks(r + width, ['MNIST','FashionMNIST','CelebA'])
+plt.legend()
+#plt.show()
+plt.savefig("./results/fid_best_comparison.png")
+#plt.clf() #clear
+
+plt.show()
 
