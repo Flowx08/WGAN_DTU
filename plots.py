9afec2c58a55ba857ecc0ddc8b34fec8df4a8768
System Administrator <root@carlos-air.students.clients.local>
2022-12-04 14:14:29 +0100

    Update

--- plots.py
+++ plots.py
@@ -4,12 +4,15 @@ import matplotlib.pyplot as plt
 
 fid_gansn = genfromtxt('results/celeba_gansn.csv', delimiter=',')[1:]
 fid_wgangp = genfromtxt('results/celeba_wgangp.csv', delimiter=',')[1:]
+fid_gan = genfromtxt('results/celeba_gan_2.csv', delimiter=',')[1:]
 print(fid_gansn)
 print(fid_wgangp)
+print(fid_gan)
 
 x = np.arange(0, len(fid_gansn), step=1) + 1
 plt.plot(x, fid_gansn, label="GAN-SN")
 plt.plot(x, fid_wgangp, label="WGAN-GP")
+plt.plot(x, fid_gan, label="GAN")
 plt.xlabel("Epoch")
 plt.ylabel("FID")
 plt.title("FID plot during training of the models")
@@ -28,7 +31,7 @@ fig = plt.figure(figsize=(6, 4), dpi=100)
 plt.bar(r, fid_wgangp, color = 'b', width = width, edgecolor = 'black', label='WGAN-GP')
 plt.bar(r + width, fid_gansn, color = 'y', width = width, edgecolor = 'black', label='GAN-SN')
 plt.bar(r + width * 2, fid_gan, color = 'r', width = width, edgecolor = 'black', label='Vanilla GAN')
-plt.xlabel("Taskset")
+plt.xlabel("Dataset")
 plt.ylabel("FID")
 plt.title("FID value of our models on MNIST, FashionMNIST\nand CelebA datasets")
 plt.xticks(r + width, ['MNIST','FashionMNIST','CelebA'])
